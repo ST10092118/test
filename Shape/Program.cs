25848a792169050a37aa2a7577494881cbda321a
ST10092118 <104516699+ST10092118@users.noreply.github.com>
2023-04-24 15:50:21 +0200

    Create Program.cs
    I calculated the area of the square

--- Shape/Program.cs
+++ Shape/Program.cs
@@ -4,10 +4,9 @@
     {
         static void Main(string[] args)
         {
-            double length = 5.0; // Length of the rectangle
-            double width = 3.0; // Width of the rectangle
-            double area = length * width; // Formula to calculate the area
-            Console.WriteLine("The area of the rectangle is: " + area); // Print the area
+            int SideLength = 5;
+            int area = SideLength * SideLength;
+            Console.WriteLine("The area of a square is: " + SideLength
         }
     }
 }
